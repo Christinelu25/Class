db5e6cda225f2614bbb8a9bdda69274e5462c4e2
Christine Joy Lu <111473122+Christinelu25@users.noreply.github.com>
2022-09-01 10:44:44 +0800

    Add files via upload

---
+++ Gear.java
@@ -0,0 +1,13 @@
+public class Gear {
+    //Properties
+    String name;
+    int defense;
+    float weight;
+    String description;
+    String rarity;
+    boolean hasElmentResistance;
+
+    public void showDescription(){
+        System.out.println("Description"+ description);
+    }
+}
---
+++ Weapon.java
@@ -0,0 +1,14 @@
+public class Weapon 
+{
+    int damage;
+    float weight;
+    String name;
+    String description;
+    String rarity;
+    boolean hasElementalDamage;
+
+    public void showDescription () {;
+    System.out.println("Description:" + description);
+    }
+    
+}
